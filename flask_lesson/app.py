d26e7efd3baed63c2f70a532a0c0c5fb60b32f78
Farooq Mahmud <f>
2020-05-02 22:07:38 -0700

    Added add new post form.

--- flask_lesson/app.py
+++ flask_lesson/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 import flask_lesson.db as db
 
@@ -20,6 +20,22 @@ def get_post(post_id: int):
         )
 
     return render_template("post.jinja2", post=post)
+
+
+@app.route("/posts/new")
+def new_post():
+    return render_template("new_post.jinja2")
+
+
+@app.route("/posts/add")
+def add_post():
+    title = request.args.get("title")
+    content = request.args.get("content")
+    post_id = len(db.posts)
+
+    db.posts[post_id] = {"id": post_id, "title": title, "content": content}
+
+    return redirect(url_for("get_post", post_id=post_id))
 
 
 if __name__ == "__main__":
--- flask_lesson/db.py
+++ flask_lesson/db.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict
 
 posts: Dict[int, Any] = {
-    0: {"title": "Hello World", "content": "This is my first blog post!"}
+    0: {"id": 0, "title": "Hello World", "content": "This is my first blog post!"}
 }
