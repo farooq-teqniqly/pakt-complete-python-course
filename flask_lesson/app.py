dd6e66d04bcd535d17c94bc7a29d529b75bb963b
Farooq Mahmud <f>
2020-05-02 21:28:28 -0700

    Render html

--- flask_lesson/app.py
+++ flask_lesson/app.py
@@ -1,20 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
 import flask_lesson.db as db
-import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello world!"
+    return render_template("welcome.html")
 
 
 @app.route("/posts/<int:post_id>")
 def get_post(post_id: int):
     post = db.posts.get(post_id)
 
-    return json.dumps(post)
+    if post is None:
+        return render_template("404.html")
+
+    return render_template("post.html", post=post)
 
 
 if __name__ == "__main__":
