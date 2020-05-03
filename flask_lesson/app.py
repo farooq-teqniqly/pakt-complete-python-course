cfbae80b8f6ccf2f79d7a5abacf481d054752200
Farooq Mahmud <f>
2020-05-02 22:19:14 -0700

    Refactoring.

--- flask_lesson/app.py
+++ flask_lesson/app.py
@@ -22,20 +22,18 @@ def get_post(post_id: int):
     return render_template("post.jinja2", post=post)
 
 
-@app.route("/posts/new")
-def new_post():
-    return render_template("new_post.jinja2")
-
-
-@app.route("/posts/add", methods=["post"])
+@app.route("/posts/add", methods=["POST", "GET"])
 def add_post():
-    title = request.form.get("title")
-    content = request.form.get("content")
-    post_id = len(db.posts)
 
-    db.posts[post_id] = {"id": post_id, "title": title, "content": content}
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+        post_id = len(db.posts)
+        db.posts[post_id] = {"id": post_id, "title": title, "content": content}
 
-    return redirect(url_for("get_post", post_id=post_id))
+        return redirect(url_for("get_post", post_id=post_id))
+
+    return render_template("new_post.jinja2")
 
 
 if __name__ == "__main__":
